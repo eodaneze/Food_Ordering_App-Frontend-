7f8e71dd7ca299086ae156fb19bf51e57330e02d
eodaneze <ezealiokechukwu1999@gmail.com>
2023-12-30 15:30:43 -0800

    completed the top nav section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
+import TopNav from "./components/TopNav"
 
 
 const App = () => {
   return (
     <div>
-       <h1 className="text-3xl font-bold underline text-center">
-      Hello world!
-    </h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora non voluptas eveniet, sed maiores dolor. Omnis, aspernatur est! Ullam nobis totam odit et magni quibusdam omnis. Recusandae dolorem nostrum repellat deserunt laudantium. Dolores eligendi recusandae numquam sequi voluptatem, consequatur placeat excepturi velit aperiam nam. Excepturi laboriosam eum assumenda sapiente fuga molestias. Numquam, ullam! Officiis dignissimos maiores labore doloribus ratione temporibus cumque? Voluptate distinctio numquam nam fugit repellendus temporibus in pariatur vel sit magnam impedit enim quia deserunt, assumenda obcaecati ex eius possimus velit sequi dicta voluptates non, facere praesentium. Aut animi quae dolor voluptates vel aperiam optio possimus reiciendis numquam.</p>
+       <TopNav />
     </div>
   )
 }
---
+++ src/components/TopNav.jsx
@@ -0,0 +1,16 @@
+const TopNav = () => {
+  return (
+    <div className="bg-[#431407] p-3">
+        <div className="text-white flex align-center justify-around container font-semibold">
+             <p>English</p>
+             <p>Free Delivery Over $50 Claim Offer</p>
+             <div className="flex">
+                 <p>Help</p>
+                 <p>Contact</p>
+             </div>
+        </div>
+    </div>
+  )
+}
+
+export default TopNav
